0187e34b06e2f201695fb3b0124f8053f7d24f49
Tayfun Yirdem <tayfunyirdem@gmail.com>
2025-02-08 14:05:03 +0300

    init

---
+++ TBL/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.RegularExpressions;
+
+var lines = File.ReadAllLines(@"C:\Program Files (x86)\Championship Manager 01-02\benchresult.txt");
+var tabbedLines = (from line in lines where line.Contains("PAS Giannina") select line.Replace(" ", "\t")).ToList();
+var formattedLines = tabbedLines.Select(line => Regex.Split(line, @"\t+")).Select(parts => string.Join(",", parts).TrimEnd(',')).ToList();
+const string path = @"C:\Program Files (x86)\Championship Manager 01-02\BenchResultFormatted.xls";
+File.WriteAllLines(path, formattedLines);
+Console.WriteLine("Operation is completed.");
+Console.ReadLine();
